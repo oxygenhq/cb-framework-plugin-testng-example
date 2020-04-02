63de64a9536015b155115ce67965c237c258f814
megager <dimonzalis@gmail.com>
2020-04-02 12:49:40 +0300

    Add custom step

--- src/test/java/Unit/UnitTest.java
+++ src/test/java/Unit/UnitTest.java
@@ -1,20 +1,25 @@
 package Unit;
 
+import io.cloudbeat.testng.CbTestNg;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners(io.cloudbeat.testng.Plugin.class)
-public class UnitTest {
+public class UnitTest extends CbTestNg {
 
     @Test(groups = {"success", "unit"})
     public void successEqualityTest() {
+        startStep("Assertion");
         Assert.assertEquals(4, 2+2);
+        endStep("Assertion");
     }
 
     @Test(groups = {"fail", "unit"})
     public void failEqualityTest() {
+        startStep("Assertion");
         Assert.assertEquals(3, 1+1);
+        endStep("Assertion");
     }
 
 }
