d5d6dfe2278e088de315be16c73aa1d8d7f2c8d8
N. Dimer <ndimer@cloudbeat.io>
2020-05-05 19:10:29 +0200

    Improve stability and testing scope of addToCartAndCheckout test method.

--- src/test/java/Selenium/SeleniumTest.java
+++ src/test/java/Selenium/SeleniumTest.java
@@ -2,12 +2,16 @@ package Selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
@@ -68,13 +72,13 @@ public class SeleniumTest extends io.cloudbeat.testng.CbTestNg {
     @Test(groups = {"success", "purchase"})
     public void purchaseTShirt() {
 
-        startStep("Open web site");
+        startStep("Open the web site");
         driver.navigate().to(homeUrl);
-        endStep("Open web site");
+        endStep("Open the web site");
 
-        startStep("Select T shirts");
+        startStep("Select T-SHIRTS");
         driver.findElement(By.linkText("T-SHIRTS")).click();
-        endStep("Select T shirts");
+        endStep("Select T-SHIRTS");
 
         startStep("Filter S");
         driver.findElement(By.id("layered_id_attribute_group_1")).click();
@@ -96,9 +100,9 @@ public class SeleniumTest extends io.cloudbeat.testng.CbTestNg {
 
     @Test(groups = {"fail"})
     public void failPurchaseDress() {
-        startStep("Open web site");
+        startStep("Open the web site");
         driver.navigate().to(homeUrl);
-        endStep("Open web site");
+        endStep("Open the web site");
 
         startStep("Select Dresses");
         driver.findElement(By.linkText("DRESSES")).click();
@@ -119,9 +123,9 @@ public class SeleniumTest extends io.cloudbeat.testng.CbTestNg {
 
     @Test(groups = {"purchase", "success"})
     public void purchaseWomenClothing() {
-        startStep("Open web site");
+        startStep("Open the web site");
         driver.navigate().to(homeUrl);
-        endStep("Open web site");
+        endStep("Open the web site");
 
         startStep("Select Women clothing");
         driver.findElement(By.linkText("WOMEN")).click();
@@ -131,13 +135,13 @@ public class SeleniumTest extends io.cloudbeat.testng.CbTestNg {
         driver.findElement(By.id("layered_category_4")).click();
         endStep("Select Tops");
 
-        startStep("Select black");
+        startStep("Select Black");
         driver.findElement(By.linkText("Black (2)")).click();
-        endStep("Select black");
+        endStep("Select Black");
 
-        startStep("Select cotton");
+        startStep("Select Cotton");
         driver.findElement(By.id("layered_id_feature_5")).click();
-        endStep("Select cotton");
+        endStep("Select Cotton");
 
         startStep("Open Specials");
         driver.findElement(By.xpath("//div[@id='special_block_right']/div/div/a/span")).click();
@@ -145,57 +149,89 @@ public class SeleniumTest extends io.cloudbeat.testng.CbTestNg {
     }
 
     @Test(groups = {"success"})
-    public void addToCart() throws InterruptedException {
+    public void addToCartAndCheckout() throws InterruptedException {
         startStep("Open web site");
         driver.navigate().to(homeUrl);
-        endStep("Open web site");
+        // maximize browser window to omit screen resolution related issues
+        driver.manage().window().maximize();
+        endStep("Open the web site");
 
-        startStep("Purchase T shirt");
+        startStep("Select a T-Shirt");
 
-        startStep("Select T shirts");
+        startStep("Click on T-SHIRTS menu");
         driver.findElement(By.linkText("T-SHIRTS")).click();
-        endStep("Select T shirts");
+        endStep("Click on T-SHIRTS menu");
 
-        startStep("Click on T shirt");
-        driver.findElement(By.xpath("(//div[@id='center_column']/ul/li/div/div[2]/div[2]/a/span)[1]")).click();
-        endStep("Click on T shirt");
+        startStep("Find and select a first t-shirt in the list");
+        List<WebElement> tshirtsList = driver.findElements(By.cssSelector(".product-image-container > .product_img_link"));
+        Assert.assertTrue(tshirtsList.size() > 0, "T-Shirts list must not be empty");
+        //driver.findElement(By.xpath("(//div[@id='center_column']/ul/li/div/div[2]/div[2]/a/span)[1]")).click();
+        endStep("Find and select a first t-shirt in the list");
+        startStep("Click on the first t-shirt");
+        tshirtsList.get(0).click();
+        endStep("Click on the first t-shirt");
 
-        endStep("Purchase T shirt");
+        endStep("Select a T-Shirt");
 
         startStep("Add to cart");
-        driver.findElement(By.xpath("//div[@id='layer_cart']/div/div[2]/div[4]/a/span")).click();
+        driver.findElement(By.name("Submit")).click();
+        //driver.findElement(By.xpath("//div[@id='layer_cart']/div/div[2]/div[4]/a/span")).click();
         endStep("Add to cart");
 
-        startStep("Change quantity to 2");
-        driver.findElement(By.xpath("//a[2]/span/i")).click();
-        endStep("Change quantity to 2");
+        startStep("Assert success message popup");
+        WebElement addToCartSuccessMsgElm = driver.findElement(By.cssSelector(".layer_cart_product h2"));
+        // wait for success popup to appear
+        (new WebDriverWait(driver, 60))
+                .until(ExpectedConditions.visibilityOf(addToCartSuccessMsgElm));
+        // make sure the popup appeared after 60 sec max
+        Assert.assertTrue(addToCartSuccessMsgElm.isDisplayed(), "Popup with successfully added message must be displayed");
+        Assert.assertEquals(addToCartSuccessMsgElm.getText(), "Product successfully added to your shopping cart");
+        endStep("Assert success message popup");
 
-        startStep("Verify price for 2 objects");
-        synchronized (driver)
-        {
-            driver.wait(2000);
-        }
-        Assert.assertEquals(driver.findElement(By.id("total_price")).getText(), "$35.02");
-        endStep("Verify price for 2 objects");
+        startStep("Assert cart total is $18.51");
+        // assert the current shopping cart sum
+        WebElement currentTotalElm = driver.findElement(By.xpath("//span[@class='ajax_block_cart_total']"));
+        Assert.assertEquals(currentTotalElm.getText(), "$18.51");
+        endStep("Assert cart total is $18.51");
 
-        startStep("Insert Personal details");
-        driver.findElement(By.xpath("//div[@id='center_column']/p[2]/a/span")).click();
+        startStep("Click on Proceed to Checkout button");
+        driver.findElement(By.xpath("//a[@title='Proceed to checkout']")).click();
+        endStep("Click on Proceed to Checkout button");
+
+        // by now, we suppose to be on Shopping Cart Summary page
+        startStep("Checkout process");
+
+        startStep("Assert that the current page is Shopping Cart Summary");
+        WebElement cartTitleElm = driver.findElement(By.id("cart_title"));
+        Assert.assertTrue(cartTitleElm.getText().contains("SHOPPING-CART SUMMARY"), "Page title must contain 'SHOPPING-CART SUMMARY'");
+        endStep("Assert that the current page is Shopping Cart Summary");
+
+        // If we refer to "Proceed to checkout" button using its title, then we will get ElementNotInteractableException
+        // as multiple elements on the page match this criteria. Thus, we will use more precise css name-based xpath instead.
+
+        driver.findElement(By.xpath("//a[contains(@class, 'standard-checkout')]")).click();
+        //driver.findElement(By.xpath("//a[@title='Proceed to checkout']")).click();
+
+        startStep("Login with pre-registered user");
+        //driver.findElement(By.xpath("//div[@id='center_column']/p[2]/a/span")).click();
         driver.findElement(By.id("email")).sendKeys("erandd@yahoo.com");
         driver.findElement(By.id("passwd")).sendKeys("eran1234");
-        driver.findElement(By.xpath("//button[@id='SubmitLogin']/span")).click();
-        endStep("Insert Personal details");
+        driver.findElement(By.name("SubmitLogin")).click();
+        startStep("Login with pre-registered user");
 
-        startStep("Verify Shipping");
+        startStep("Verify shipping");
         driver.findElement(By.xpath("//div[@id='center_column']/form/p/button/span")).click();
         driver.findElement(By.id("cgv")).click();
         driver.findElement(By.xpath("//form[@id='form']/p/button/span")).click();
-        endStep("Verify Shipping");
+        endStep("Verify shipping");
 
-        startStep("Verify Payment");
+        startStep("Verify payment");
         driver.findElement(By.xpath("(//div[@id='HOOK_PAYMENT']/div/div/p/a/span)[1]")).click();
         driver.findElement(By.xpath("//p[@id='cart_navigation']/button/span")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='center_column']/h1")).getText(), "ORDER CONFIRMATION");
-        endStep("Verify Payment");
+        endStep("Verify payment");
+
+        endStep("Checkout process");
     }
 
     @Test(groups = {"fail"})
